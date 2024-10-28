04563503f91e4d9650ef26b8d197e55a593148a3
Ten Uchikawa <ten.uchikawa@gmail.com>
2024-10-28 16:10:54 +0900

    add gitignore makefile

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: tuchikaw <tuchikaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 18:41:31 by tuchikaw          #+#    #+#             */
-/*   Updated: 2024/10/28 02:01:52 by tuchikaw         ###   ########.fr       */
+/*   Updated: 2024/10/28 16:09:01 by tuchikaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ Point::Point(float x, float y) : _x(x), _y(y)
 }
 Point::Point(const Point &point)
 {
+	this->operator=(point);
 }
+
 Point &Point::operator=(const Point &point)
 {
 	if (this != &point)
